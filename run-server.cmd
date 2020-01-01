7aa6262bcb766f4e8c0a73a681ab623009c35d22
Caesar Ladion <caesar.ladion@gmail.com>
2019-12-31 17:01:52 -0800

    added catalog service flag

--- run-server.cmd
+++ run-server.cmd
@@ -1,4 +1,3 @@
 cd cmd/server
 go build .
-.\server.exe -grpc-port=9091 -http-port=8080 -db-host=blah -db-user=dev -db-password=dev-user5 -db-schema=checkout 
-  
+.\server.exe -grpc-port=9091 -http-port=8080 -db-host=blah -db-user=dev -db-password=dev-user5 -db-schema=checkout -catalog-service-address=localhost:9090
